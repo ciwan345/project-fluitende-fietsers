9767205eef74c2d588536f9de7b7ed7927cbe6db
Milan djilani <milandjilani@gmail.com>
2026-09-17 01:09:59 +0200

    werk van vandaag

---
+++ fietser.js
@@ -0,0 +1,30 @@
+function tijdweergave() {
+
+    const nu = new Date();
+    const tijd = nu.toLocaleTimeString("nl-NL", {
+        hour: "2-digit",
+        minute: "2-digit"
+    });
+
+    const uren = nu.getHours();
+    const zin = document.getElementById("opendicht");
+    const dot = document.querySelector(".status-dot");
+
+
+    if (uren >= 10 && uren < 18) {
+        zin.textContent = "winkel geopend";
+        zin.style.color = "green";
+        dot.style.backgroundColor = "green"; 
+    } else {
+        zin.textContent = "winkel gesloten";
+        zin.style.color = "crimson";
+        dot.style.backgroundColor = "red"; 
+
+      
+    }
+
+    document.getElementById("tijdstatus").textContent = tijd + " — ";
+}
+
+setInterval(tijdweergave, 1000);
+tijdweergave();
