e55973cdb947ef92df4b510b7f2966fcf3e8b0ba
jowan <jowan@summa.college.nl>
2026-09-19 02:06:25 +0200

    update 2

--- fietser.js
+++ fietser.js
@@ -28,3 +28,79 @@ function tijdweergave() {
 
 setInterval(tijdweergave, 1000);
 tijdweergave();
+
+
+
+//..................Foto carousel maken op de hoofdpagina ................................
+
+
+
+const images = document.querySelectorAll(".carousel-image");
+const dots = document.querySelectorAll(".dot");
+
+const nextButton = document.querySelector(".next");
+const prevButton = document.querySelector(".prev");
+
+let currentImage = 0;
+
+
+function showImage(index) {
+
+    images.forEach(function(image) {
+        image.classList.remove("active");
+    });
+
+    dots.forEach(function(dot) {
+        dot.classList.remove("active");
+    });
+
+    images[index].classList.add("active");
+    dots[index].classList.add("active");
+
+    currentImage = index;
+}
+
+
+/* Volgende foto */
+
+nextButton.addEventListener("click", function() {
+
+    currentImage++;
+
+    if (currentImage >= images.length) {
+        currentImage = 0;
+    }
+
+    showImage(currentImage);
+
+});
+
+
+/* Vorige foto */
+
+prevButton.addEventListener("click", function() {
+
+    currentImage--;
+
+    if (currentImage < 0) {
+        currentImage = images.length - 1;
+    }
+
+    showImage(currentImage);
+
+});
+
+
+/* Automatisch wisselen */
+
+setInterval(function() {
+
+    currentImage++;
+
+    if (currentImage >= images.length) {
+        currentImage = 0;
+    }
+
+    showImage(currentImage);
+
+}, 4000);
